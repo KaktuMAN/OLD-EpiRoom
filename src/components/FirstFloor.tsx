61d8eff4cf6d2da6132858f5a7444d6d0a6da0be
Nicolas PECHART <nicolas.pechart@epitech.eu>
2023-10-31 08:51:07 +0100

    :alembic: Floor display from a SVG component

---
+++ src/components/FirstFloor.tsx
@@ -0,0 +1,21 @@
+import {FC} from "react";
+import { Room } from "@types/room";
+
+interface FirstFloorProps {
+  roomData: Room[];
+}
+
+const FirstFloor: FC<FirstFloorProps> = (props) => {
+  const { roomData } = props;
+  const statuses = ["free", "occupied", "reserved"]
+  return (
+    <>
+      <svg width="1679" height="920">
+        <use href={"/rooms/1/WALLS.svg#VUE_3D-2"}/>
+        <use href={"/rooms/1/0-11-Kanojedo.svg#ROOMS"} x={535} y={215}/>
+      </svg>
+    </>
+  );
+};
+
+export default FirstFloor

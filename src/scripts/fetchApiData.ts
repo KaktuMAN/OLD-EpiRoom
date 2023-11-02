6b36f9a05aafe4c6e73f6c07eef72f244bd75d13
Nicolas PECHART <nicolas.pechart@epitech.eu>
2023-11-02 19:37:38 +0100

    :sparkles: Detect multiple room from the API

--- src/scripts/fetchApiData.ts
+++ src/scripts/fetchApiData.ts
@@ -1,6 +1,24 @@
 import { Activity } from "@customTypes/activity";
 import { Room } from "@customTypes/room"
 
+/**
+ * Store data from API response in room object
+ * @param rooms
+ * @param activity
+ * @param roomsNames
+ */
+function storeDataMultipleRooms(rooms: Room[], activity: Activity, roomsNames: string[]): void {
+  roomsNames.forEach((roomName) => {
+    let room = rooms.find((room) => room.intra_name === roomName)
+    if (!room) {
+      console.error(`Room ${roomName} not found in rooms list.`);
+      return;
+    }
+    room.activities.push(activity)
+    room.activities.sort((a, b) => a.start.getTime() - b.start.getTime())
+  })
+}
+
 export default function fetchApiData(roomsComp: Room[]): void {
   const apiData = fetch(`https://lille-epirooms.epitest.eu/?date=${new Date().toISOString().slice(0, 10)}`)
     .then((response) => {
@@ -16,13 +34,8 @@ export default function fetchApiData(roomsComp: Room[]): void {
     data.forEach((activityData: APIResponse) => {
       if (!activityData.room || !activityData.room.type || activityData.instance_location != "FR/LIL" ) return;
 
-      const room = roomsComp.find((room) => room.intra_name === activityData.room.code);
+      let room = roomsComp.find((room) => room.intra_name === activityData.room.code);
       let activity: Activity = {title: "", start: new Date(), end: new Date()}
-
-      if (!room) {
-        console.warn(`Room ${activityData.room.code} not found in rooms list.`);
-        return;
-      }
 
       if (activityData.id_calendar) {
         activity.title = activityData.title
@@ -36,8 +49,33 @@ export default function fetchApiData(roomsComp: Room[]): void {
         activity.end = new Date(activityData.end)
       }
 
-      room.activities.push(activity)
-      room.activities.sort((a, b) => a.start.getTime() - b.start.getTime())
+      if (!room && activityData.room.code.split('/')[3].startsWith("S-21abc")) {
+        storeDataMultipleRooms(roomsComp, activity, ["FR/LIL/Hopital-Militaire/S-21a-Denis", "FR/LIL/Hopital-Militaire/S-21b-MacAlistair", "FR/LIL/Hopital-Militaire/S-21c-Ritchie"])
+        return;
+      }
+
+      if (!room && activityData.room.code.split('/')[3].startsWith("S-23ab")) {
+        storeDataMultipleRooms(roomsComp, activity, ["FR/LIL/Hopital-Militaire/S-23a-Hedy-Lamarr", "FR/LIL/Hopital-Militaire/S-23b-Al-Jazari"])
+        return;
+      }
+
+      if (!room && activityData.room.code.split('/')[3].startsWith("S-25ab")) {
+        storeDataMultipleRooms(roomsComp, activity, ["FR/LIL/Hopital-Militaire/S-25a-Gwen", "FR/LIL/Hopital-Militaire/S-25b-Barzey"])
+        return;
+      }
+
+      if (!room && activityData.room.code.split('/')[3].startsWith("S-33ab")) {
+        storeDataMultipleRooms(roomsComp, activity, ["FR/LIL/Hopital-Militaire/S-33a-Deep-Blue", "FR/LIL/Hopital-Militaire/S-33a-Blue-Brain"])
+        return;
+      }
+
+      if (!room) {
+        console.warn(`Room ${activityData.room.code} not found in rooms list.`);
+        return;
+      } else if (room) {
+        room.activities.push(activity)
+        room.activities.sort((a, b) => a.start.getTime() - b.start.getTime())
+      }
     })
   }).catch((error) => {
     console.error(`API FETCH FAILED: ${error}`)

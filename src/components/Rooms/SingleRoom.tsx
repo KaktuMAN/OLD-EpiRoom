99789aaf94a1da74a1a9465af3e24e6149e13070
Nicolas PECHART <nicolas.pechart@epitech.eu>
2023-11-02 20:02:42 +0100

    :truck: Ordered Components

--- src/components/Rooms/SingleRoom.tsx
+++ src/components/Rooms/SingleRoom.tsx
@@ -10,18 +10,23 @@ interface SingleRoomProps {
 
 const SingleRoom: FC<SingleRoomProps> = (props) => {
   const { roomData, svg_path } = props;
-  const [status, setStatus] = useState(0);
   const [loaded, setLoaded] = useState(false);
   useEffect(() => {
+    const waitLoad = setInterval(() => {
+      if (roomData.loaded) {
+        clearInterval(waitLoad);
+        setLoaded(true);
+      }
+    }, 1000);
     const interval = setInterval(() => {
       if (roomData.activities.length === 0) {
-        setStatus(2);
+        roomData.setStatus(2);
       } else if (roomData.activities[0].start.valueOf() < Date.now() && roomData.activities[0].end.valueOf() > Date.now()) {
-        setStatus(0);
+        roomData.setStatus(0);
       } else if (roomData.activities[0].start.valueOf() > Date.now()) {
-        setStatus(1);
+        roomData.setStatus(1);
       } else {
-        setStatus(2);
+        roomData.setStatus(2);
         roomData.activities.pop();
       }
     }, 1000);
@@ -32,20 +37,26 @@ const SingleRoom: FC<SingleRoomProps> = (props) => {
   return (
     <>
       <div className="backdrop-blur-md bg-white/30">
-        {!loaded && (
-          <Skeleton variant={"rounded"} width={150} height={120}/>
-        )}
-        <Image
-          src={svg_path}
-          alt={""}
-          width={150}
-          height={100}
-          className={["occupied", "reserved", "free"][status]}
-          onLoadingComplete={() => setLoaded(true)}
-        />
-        <p>
-          {roomData.display_name}
-        </p>
+        {!loaded ? (
+          <>
+            <Skeleton variant={"rounded"} width={200} height={120}/>
+            <br/>
+            <Skeleton variant={"rounded"} width={200} height={25}/>
+          </>
+        ) : (
+          <>
+            <Image
+              src={svg_path}
+              alt={""}
+              width={200}
+              height={100}
+              className={["occupied", "reserved", "free"][roomData.status]}
+            />
+            <p>
+              {roomData.display_name}
+            </p>
+          </>
+          )}
       </div>
     </>
   );

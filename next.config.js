da12688ced90eecd30ca42bfd97b71f6c5815912
Nicolas <nicolas.pechart@epitech.eu>
2024-01-06 17:26:13 +0100

    :wrench: Change config to deploy on vercel

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  output: "export",
-  basePath: "/EpiRoom",
+  swcMinify: true
 }
 
 module.exports = nextConfig

ffe9edf961b3561c516d6a891fe86ebc52c11c70
Nicolas <nicolas.pechart@epitech.eu>
2024-02-01 11:45:48 +0100

    :construction: Adding PWA app

--- next.config.js
+++ next.config.js
@@ -4,4 +4,11 @@ const nextConfig = {
   swcMinify: true
 }
 
-module.exports = nextConfig
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA(nextConfig);

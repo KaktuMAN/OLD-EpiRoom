1cda4efda50947a602d697818c0b69abe8a666a2
Nicolas <nicolas.pechart@epitech.eu>
2024-03-11 16:57:51 +0100

    :memo: Added Swagger documentation

--- api/src/main/java/com/epiroom/api/model/Campus.java
+++ api/src/main/java/com/epiroom/api/model/Campus.java
@@ -1,14 +1,20 @@
 package com.epiroom.api.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "campus")
 public class Campus {
     @Id
+    @NotNull
+    @Size(min = 3, max = 3)
     private String code;
 
     @Column(name = "name")
+    @NotNull
+    @Size(max = 50)
     private String name;
 
     @Column(name = "main_floor")

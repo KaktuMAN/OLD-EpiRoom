f11862209ba0ac522170db7b24c37fd7d6a8e4af
Nicolas <nicolas.pechart@epitech.eu>
2024-04-11 16:24:22 +0200

    :bug: Wrong server URL in prod

--- api/src/main/java/com/epiroom/api/config/OpenAPIConfig.java
+++ api/src/main/java/com/epiroom/api/config/OpenAPIConfig.java
@@ -4,12 +4,18 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Collections;
+
 @Configuration
 public class OpenAPIConfig {
+
+    @Value("${environment.api-base-url}")
+    private String API_URL;
 
     @Bean
     public OpenAPI myOpenAPI() {
@@ -23,13 +29,14 @@ public class OpenAPIConfig {
                 .title("EpiRoom API")
                 .version("0.3")
                 .contact(contact)
-                .description("This API is a wrapper around the Epitech Intranet API. (Mainly used for EpiRoom)")
+                .description("This API is used for EpiRoom")
                 .license(mitLicense);
 
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList("oauth2");
+        Server server = new Server();
+        server.setUrl(API_URL);
 
         return new OpenAPI()
-                .info(info)
-                .addSecurityItem(securityRequirement);
+            .info(info)
+            .servers(Collections.singletonList(server));
     }
 }
